b148474bbbc4c9d7556560c77d40fc014bcb9f62
tutajm <mtutaj@mcw.edu>
2022-02-02 10:38:50 -0600

    updated oracle jdbc dep

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -11,7 +11,7 @@ if [ "$SERVER" = "REED" ]; then
 fi
 
 cd $APPDIR
-java -jar -Dspring.config=$APPDIR/../properties/default_db.xml \
+java -jar -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar "$@" > run.log 2>&1
 
