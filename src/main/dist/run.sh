5878e5af364641e6fde8d45e870fd45af27a89af
tutajm <mtutaj@mcw.edu>
2022-03-07 15:36:48 -0600

    renamed to 'strain-rso-annotation-pipeline'

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# shell script to run StrainRsoAnnotation
+# shell script to run strain-rso-annotation-pipeline
 . /etc/profile
 
-APPNAME=StrainRsoAnnotation
+APPNAME=strain-rso-annotation-pipeline
 APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
@@ -15,7 +15,7 @@ java -jar -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar "$@" > run.log 2>&1
 
-mailx -s "[$SERVER] StrainRsoAnnotation pipeline OK" $EMAIL_LIST < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] strain rso annotation pipeline OK" $EMAIL_LIST < $APPDIR/logs/summary.log
 
 /home/rgddata/pipelines/OntologyLoad/run_single.sh RS -skip_download &
 
