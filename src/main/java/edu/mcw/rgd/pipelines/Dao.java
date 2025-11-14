6b0e8a0ef668e8e6d45a4fb45f0f2b4dbe3309b2
tutajm <mtutaj@mcw.edu>
2025-11-14 14:38:08 -0600

    refactored sql for better readability

--- src/main/java/edu/mcw/rgd/pipelines/Dao.java
+++ src/main/java/edu/mcw/rgd/pipelines/Dao.java
@@ -29,43 +29,34 @@ public class Dao {
     }
 
     public int updateStrainRsoAnnotations() throws Exception {
-        String sql = "UPDATE FULL_ANNOT fa\n" +
-                "SET (\n" +
-                "    term," +
-                "    object_symbol," +
-                "    object_name," +
-                "    last_modified_date" +
-                "  )\n" +
-                "  =\n" +
-                "  (SELECT ot.TERM,\n" +
-                "    st.STRAIN_SYMBOL,\n" +
-                "    st.FULL_NAME,\n" +
-                "    sysdate\n" +
-                "  FROM ONT_TERMS ot,\n" +
-                "    strains st,\n" +
-                "    rgd_ids i\n" +
-                "  WHERE \n" +
-                "   fa.TERM_ACC              = ot.TERM_ACC\n" +
-                "  AND fa.ANNOTATED_OBJECT_RGD_ID = st.RGD_ID\n" +
-                "  AND fa.ANNOTATED_OBJECT_RGD_ID = i.RGD_ID\n" +
-                "  AND i.object_status = 'ACTIVE'\n" +
-                "  AND ot.is_obsolete = 0\n" +
-                "  )\n" +
-                "WHERE fa.LAST_MODIFIED_BY=?\n" +
-                "AND EXISTS\n" +
-                "  (SELECT ot.TERM,\n" +
-                "    st.STRAIN_SYMBOL,\n" +
-                "    st.FULL_NAME,\n" +
-                "    sysdate\n" +
-                "  FROM ONT_TERMS ot,\n" +
-                "    strains st,\n" +
-                "    rgd_ids i\n" +
-                "  WHERE fa.TERM_ACC              = ot.TERM_ACC\n" +
-                "  AND fa.ANNOTATED_OBJECT_RGD_ID = st.RGD_ID\n" +
-                "  AND fa.ANNOTATED_OBJECT_RGD_ID = i.RGD_ID\n" +
-                "  AND i.object_status = 'ACTIVE'\n" +
-                "  AND ot.is_obsolete = 0\n" +
-                ")";
+        String sql = """
+            UPDATE FULL_ANNOT fa
+            SET (term, object_symbol, object_name, last_modified_date)
+              =
+              (SELECT ot.TERM, st.STRAIN_SYMBOL, st.FULL_NAME, sysdate
+              FROM ONT_TERMS ot,
+                strains st,
+                rgd_ids i
+              WHERE
+               fa.TERM_ACC              = ot.TERM_ACC
+              AND fa.ANNOTATED_OBJECT_RGD_ID = st.RGD_ID
+              AND fa.ANNOTATED_OBJECT_RGD_ID = i.RGD_ID
+              AND i.object_status = 'ACTIVE'
+              AND ot.is_obsolete = 0
+              )
+            WHERE fa.LAST_MODIFIED_BY=?
+            AND EXISTS
+              (SELECT ot.TERM, st.STRAIN_SYMBOL, st.FULL_NAME, sysdate
+              FROM ONT_TERMS ot,
+                strains st,
+                rgd_ids i
+              WHERE fa.TERM_ACC              = ot.TERM_ACC
+              AND fa.ANNOTATED_OBJECT_RGD_ID = st.RGD_ID
+              AND fa.ANNOTATED_OBJECT_RGD_ID = i.RGD_ID
+              AND i.object_status = 'ACTIVE'
+              AND ot.is_obsolete = 0
+            )
+            """;
         return annotationDAO.update(sql, getCreatedBy());
     }
 
@@ -89,55 +80,32 @@ public class Dao {
     }
 
     public int insertStrainRsoAnnotations() throws Exception {
-        String sql = "INSERT INTO FULL_ANNOT (\n" +
-            "    full_annot_key,\n" +
-            "    term,\n" +
-            "    annotated_object_rgd_id,\n" +
-            "    rgd_object_key,\n" +
-            "    data_src,\n" +
-            "    object_symbol,\n" +
-            "    ref_rgd_id,\n" +
-            "    evidence,\n" +
-            "    aspect,\n" +
-            "    object_name,\n" +
-            "    created_date,\n" +
-            "    last_modified_date,\n" +
-            "    term_acc,\n" +
-            "    created_by,\n" +
-            "    last_modified_by\n" +
-            "  )\n" +
-            "SELECT FULL_ANNOT_SEQ.nextval,\n" +
-            "  ot.TERM,\n" +
-            "  st.RGD_ID,\n" +
-            "  5,\n" +
-            "  'RGD',\n" +
-            "  st.STRAIN_SYMBOL,\n" +
-            "  ?,\n" +
-            "  'IEA',\n" +
-            "  'S',\n" +
-            "  st.FULL_NAME,\n" +
-            "  sysdate,\n" +
-            "  sysdate,\n" +
-            "  ot.TERM_ACC,\n" +
-            "  ?,\n" +
-            "  ?\n" +
-            "FROM ONT_TERMS ot,\n" +
-            "  ONT_SYNONYMS os,\n" +
-            "  strains st,\n" +
-            "  RGD_IDS i\n" +
-            "WHERE os.SYNONYM_NAME LIKE 'RGD ID:%'\n" +
-            "AND to_number(SUBSTR(os.SYNONYM_NAME,9, 100)) = st.RGD_ID\n" +
-            "AND st.rgd_id = i.rgd_id\n" +
-            "AND ot.TERM_ACC = os.TERM_ACC\n" +
-            "AND i.OBJECT_STATUS = 'ACTIVE'\n" +
-            "  AND ot.is_obsolete = 0\n" +
-            "AND NOT EXISTS\n" +
-            "  (SELECT fa.FULL_ANNOT_KEY\n" +
-            "  FROM FULL_ANNOT fa\n" +
-            "  WHERE fa.TERM_ACC = ot.TERM_ACC\n" +
-            "  AND fa.ANNOTATED_OBJECT_RGD_ID = st.rgd_id\n" +
-            "  AND fa.created_by = ?\n" +
-            ")";
+        String sql = """
+            INSERT INTO FULL_ANNOT (
+                full_annot_key, term, annotated_object_rgd_id, rgd_object_key, data_src,
+                object_symbol, ref_rgd_id, evidence, aspect, object_name,
+                created_date, last_modified_date, term_acc, created_by, last_modified_by
+              )
+            SELECT FULL_ANNOT_SEQ.nextval, ot.TERM, st.RGD_ID, 5, 'RGD',
+              st.STRAIN_SYMBOL, ?, 'IEA', 'S', st.FULL_NAME,
+              sysdate, sysdate, ot.TERM_ACC, ?, ?
+            FROM ONT_TERMS ot,
+              ONT_SYNONYMS os,
+              strains st,
+              RGD_IDS i
+            WHERE os.SYNONYM_NAME LIKE 'RGD ID:%'
+              AND to_number(SUBSTR(os.SYNONYM_NAME,9, 100)) = st.RGD_ID
+              AND st.rgd_id = i.rgd_id
+              AND ot.TERM_ACC = os.TERM_ACC
+              AND i.OBJECT_STATUS = 'ACTIVE'
+              AND ot.is_obsolete = 0
+              AND NOT EXISTS
+              (SELECT fa.FULL_ANNOT_KEY FROM FULL_ANNOT fa
+               WHERE fa.TERM_ACC = ot.TERM_ACC
+                 AND fa.ANNOTATED_OBJECT_RGD_ID = st.rgd_id
+                AND fa.created_by = ?
+               )
+            """;
         return annotationDAO.update(sql, getRefRgdId(), getCreatedBy(), getCreatedBy(), getCreatedBy());
     }
 
